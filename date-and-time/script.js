4a443ad70376b5a216ef5198b8fb2835319b8e26
Manuel Ogomigo <manuelogomigo@gmail.com>
2023-07-16 19:45:20 +0100

    Changes to time

--- date-and-time/script.js
+++ date-and-time/script.js
@@ -1,14 +1,16 @@
 function getCurrentTime(element) {
-            const timeZone = element.getAttribute('ct-date-timezone') || Intl.DateTimeFormat().resolvedOptions().timeZone;
-            const mode = element.getAttribute('ct-date-mode') || '12hr';
-            const options = { timeZone, hour12: (mode === '12hr'), hour: 'numeric', minute: 'numeric', second: 'numeric' };
-            const currentTime = new Date().toLocaleTimeString([], options);
-            element.textContent = currentTime;
-            setInterval(() => {
-                const currentTime = new Date().toLocaleTimeString([], options);
-                element.textContent = currentTime;
-            }, 1000);
-        }
+    const timeZone = element.getAttribute('ct-date-timezone') || Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const mode = element.getAttribute('ct-date-mode') || '12hr';
+    const options = { timeZone, hour12: (mode === '12hr'), hour: 'numeric', minute: 'numeric', second: 'numeric' };
 
-        const elements = document.querySelectorAll('[ct-date-time]');
-        elements.forEach((element) => getCurrentTime(element));
+    function updateTime() {
+        const currentTime = new Date().toLocaleTimeString([], options);
+        element.textContent = currentTime;
+    }
+
+    updateTime();
+    setInterval(updateTime, 1000);
+}
+
+const elements = document.querySelectorAll('[ct-date-timezone]');
+elements.forEach((element) => getCurrentTime(element));
