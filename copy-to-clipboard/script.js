32e09a0a74b1a5ce9b21635df72f1b8b847086eb
Manuel Ogomigo <manuelogomigo@gmail.com>
2023-07-22 08:44:50 +0100

    Add copy command in Copy to Clipboard

--- copy-to-clipboard/script.js
+++ copy-to-clipboard/script.js
@@ -1,65 +1,105 @@
+function getCurrentDateTime(element) {
+    const timeZone = element.getAttribute('ct-time-date') || Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    const dateOptions = {
+        timeZone,
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+
+    function updateDateTime() {
+        const currentDate = new Date().toLocaleDateString([], dateOptions);
+
+        element.textContent = currentDate;
+    }
+
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+}
+
+const timeElements = document.querySelectorAll('[ct-time-date]');
+timeElements.forEach((element) => getCurrentDateTime(element));
+
+function getCurrentTime(element) {
+    const timeZone = element.getAttribute('ct-time-zone') || Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const mode = element.getAttribute('ct-time-mode') || '12hr';
+    const showSeconds = element.getAttribute('ct-time-seconds') !== 'false';
+    const options = {
+        timeZone,
+        hour12: (mode === '12hr'),
+        hour: 'numeric',
+        minute: 'numeric',
+        second: showSeconds ? 'numeric' : undefined, // Only include 'second' if showSeconds is true
+    };
+
+    function updateTime() {
+        const currentTime = new Date().toLocaleTimeString([], options);
+        element.textContent = currentTime;
+    }
+
+    updateTime();
+    setInterval(updateTime, 1000);
+}
+
+const zoneElements = document.querySelectorAll('[ct-time-zone]');
+zoneElements.forEach((element) => getCurrentTime(element));
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    var copyTriggers = document.querySelectorAll('[ct-copy-element^="trigger"]');
-    var copyTexts = document.querySelectorAll('[ct-copy-text^="target"]');
-  
-    copyTriggers.forEach(function (copyTrigger) {
-      var triggerIndex = copyTrigger
-        .getAttribute("ct-copy-element")
-        .replace("trigger", "");
-      var copyText = document.querySelector(
-        '[ct-copy-text="target' + triggerIndex + '"]'
-      );
-      var copyClass = copyTrigger.getAttribute("ct-copy-class");
-  
-      copyTrigger.addEventListener("click", function (event) {
-        // Use the trigger element itself as the target
-        var clickedElement = copyTrigger;
-  
+  var copyTriggers = document.querySelectorAll('[ct-copy-element^="trigger"]');
+  var copyTexts = document.querySelectorAll('[ct-copy-text^="target"]');
+
+  copyTriggers.forEach(function (copyTrigger) {
+    var triggerIndex = copyTrigger
+      .getAttribute("ct-copy-element")
+      .replace("trigger", "");
+    var copyText = document.querySelector(
+      '[ct-copy-text="target' + triggerIndex + '"]'
+    );
+    var copyClass = copyTrigger.getAttribute("ct-copy-class");
+
+    copyTrigger.addEventListener("click", function (event) {
+      // Use the trigger element itself as the target
+      var clickedElement = copyTrigger;
+
+      clickedElement.classList.add(copyClass);
+
+      // Check if the clicked element is the trigger element or its child
+      var isChildElement = false;
+      var currentElement = clickedElement;
+      while (currentElement !== copyTrigger) {
+        if (currentElement === document.body) {
+          isChildElement = false;
+          break;
+        }
+        currentElement = currentElement.parentElement;
+        if (currentElement === copyTrigger) {
+          isChildElement = true;
+          break;
+        }
+      }
+
+      if (!isChildElement) {
         clickedElement.classList.add(copyClass);
-  
-        // Check if the clicked element is the trigger element or its child
-        var isChildElement = false;
-        var currentElement = clickedElement;
-        while (currentElement !== copyTrigger) {
-          if (currentElement === document.body) {
-            isChildElement = false;
-            break;
-          }
-          currentElement = currentElement.parentElement;
-          if (currentElement === copyTrigger) {
-            isChildElement = true;
-            break;
-          }
-        }
-  
-        if (!isChildElement) {
-          clickedElement.classList.add(copyClass);
-  
-          // Create a temporary textarea element
-        var tempTextarea = document.createElement('textarea');
 
-        // Set the value of the textarea to the text content of the target element
-        tempTextarea.value = copyText.textContent;
+        // Copy the text content of the target element to the clipboard
+        var textToCopy = copyText.textContent;
+        var tempElement = document.createElement("textarea");
+        tempElement.style.position = "absolute";
+        tempElement.style.left = "-9999px";
+        tempElement.value = textToCopy;
+        document.body.appendChild(tempElement);
+        tempElement.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempElement);
 
-        // Append the textarea to the document body
-        document.body.appendChild(tempTextarea);
-
-        // Select the contents of the textarea
-        tempTextarea.select();
-
-        // Copy the selected text to the clipboard
-        document.execCommand('copy');
-
-        // Remove the temporary textarea
-        document.body.removeChild(tempTextarea);
-
-  
-          // Remove the combo class from the trigger element after 1 second
-          setTimeout(function () {
-            clickedElement.classList.remove(copyClass);
-          }, 1000);
-        }
-      });
+        // Remove the combo class from the trigger element after 1 second
+        setTimeout(function () {
+          clickedElement.classList.remove(copyClass);
+        }, 1000);
+      }
     });
   });
-  
+});
+
