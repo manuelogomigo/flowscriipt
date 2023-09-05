f1284cedd72e09a32f99520c604a6cbf15db776e
Akintola Rahmat <akintolarahmah@gmail.com>
2023-09-05 13:04:39 +0000

    Added vertical scroll snap

--- scroll-snap/script.js
+++ scroll-snap/script.js
@@ -1,34 +1,9 @@
 const sections = document.querySelectorAll("[ct-scroll-section]");
-let currentSectionIndex = 0;
 
-const options = {
-  threshold: 0.4,
-};
-
-let scrolling = false;
-const scrollSnap = (entries, observer)=> {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting && entry.intersectionRatio >= 0.6 && !scrolling) {
-      const index = [...sections].findIndex(
-        (section) => section.id === entry.target.id,
-      );
-      currentSectionIndex = index;
-      scrolling = true;
-      VerticalScrollToSection(currentSectionIndex);
-      setTimeout(() => {
-        scrolling = false;
-      }, 1000);
-    }
-  });
-};
-
-const observer = new IntersectionObserver(scrollSnap, options);
-
-sections.forEach((section) => {
-  observer.observe(section);
+sections.forEach(function (section) {
+  section.style.scrollSnapAlign = "start";
 });
-
-function VerticalScrollToSection(index) {
-  const targetSection = sections[index];
-  targetSection.scrollIntoView({ behavior: "auto", block: "center" });
-}
+const root = document.documentElement;
+root.style.scrollSnapType = "y mandatory";
+root.style.overflowY = "scroll";
+root.style.scrollSnapPointsY = "repeat(100vh)";
