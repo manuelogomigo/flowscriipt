080cbc5a42e8c1e4920b652c3a4956fd3a184e1b
Lawwee <lawalmohammed567@gmail.com>
2024-01-11 23:20:38 +0100

    added custom speed setting function to marquee script

--- marquee/script.js
+++ marquee/script.js
@@ -3,41 +3,46 @@ document.addEventListener("DOMContentLoaded", function () {
     applyMarquee();
 });
   
-function applyMarquee() {
-    const elements = document.querySelectorAll("[data-marquee]");
-    elements.forEach((element) => {
-        const direction = element.getAttribute("data-marquee");
-        const text = element.textContent;
+    function applyMarquee() {
+        const elements = document.querySelectorAll("[data-marquee]");
+        elements.forEach((element) => {
+            const direction = element.getAttribute("data-marquee");
+            const text = element.textContent;
+            let speed = element.getAttribute("data-speed");
+            speed = Number(speed);
 
-        const marquee = document.createElement("div");
-        marquee.style.height = "50px";
-        marquee.style.overflow = "hidden";
-        marquee.style.position = "relative";
-        marquee.style.background = "#fefefe";
-        marquee.style.color = "#333";
+            if (isNaN(speed) || speed < 1 || speed > 100) {
+                speed = 20;
+            }
 
-        const marqueeP = document.createElement("p");
-        marqueeP.innerHTML = text;
-        marqueeP.innerHTML = text;
-        marqueeP.style.position = "absolute";
-        marqueeP.style.width = "100%";
-        marqueeP.style.height = "100%";
-        marqueeP.style.margin = 0;
-        marqueeP.style.lineHeight = "50px";
-        marqueeP.style.textAlign = "center";
-        marqueeP.style.transform = "translateX(100%)";
-        if (direction === "right") {
-        marqueeP.style.animation = "scroll-right 20s linear infinite";
-        } else {
-        marqueeP.style.animation = "scroll-left 20s linear infinite";
-        }
-        marquee.appendChild(marqueeP);
-        element.parentNode.replaceChild(marquee, element);
-    })
-  }
+            const marquee = document.createElement("div");
+            marquee.style.height = "50px";
+            marquee.style.overflow = "hidden";
+            marquee.style.position = "relative";
+            marquee.style.background = "#fefefe";
+            marquee.style.color = "#333";
+
+            const marqueeP = document.createElement("p");
+            marqueeP.innerHTML = text;
+            marqueeP.style.position = "absolute";
+            marqueeP.style.width = "100%";
+            marqueeP.style.height = "100%";
+            marqueeP.style.margin = 0;
+            marqueeP.style.lineHeight = "50px";
+            marqueeP.style.textAlign = "center";
+            marqueeP.style.transform = "translateX(100%)";
+            if (direction === "right") {
+            marqueeP.style.animation = "scroll-right " + speed + "s linear infinite";
+            } else {
+            marqueeP.style.animation = "scroll-left " + speed + "s linear infinite";
+            }
+            marquee.appendChild(marqueeP);
+            element.parentNode.replaceChild(marquee, element);
+        })
+    }
   
   function createKeyframes() {
-    var style = document.createElement("style");
+    const style = document.createElement("style");
     style.innerHTML = `
       @keyframes scroll-left {
         from {
