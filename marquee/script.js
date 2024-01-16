d3a7bc624085155cf06ca506920e16ff19a0e95c
Lawwee <lawalmohammed567@gmail.com>
2024-01-16 14:31:01 +0100

    removed marquee styling

--- marquee/script.js
+++ marquee/script.js
@@ -16,21 +16,10 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             const marquee = document.createElement("div");
-            marquee.style.height = "50px";
-            marquee.style.overflow = "hidden";
-            marquee.style.position = "relative";
-            marquee.style.background = "#fefefe";
-            marquee.style.color = "#333";
 
             const marqueeP = document.createElement("p");
             marqueeP.innerHTML = text;
-            marqueeP.style.position = "absolute";
-            marqueeP.style.width = "100%";
-            marqueeP.style.height = "100%";
-            marqueeP.style.margin = 0;
-            marqueeP.style.lineHeight = "50px";
-            marqueeP.style.textAlign = "center";
-            marqueeP.style.transform = "translateX(100%)";
+            
             if (direction === "right") {
             marqueeP.style.animation = "scroll-right " + speed + "s linear infinite";
             } else {
