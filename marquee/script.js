e070ec1a9d21a098160ff7217984ad8f28c7a477
Lawwee <lawalmohammed567@gmail.com>
2024-02-05 12:19:25 +0100

    gave cloning ability to marquee script

--- marquee/script.js
+++ marquee/script.js
@@ -9,22 +9,39 @@ function applyMarquee() {
     const direction = element.getAttribute("data-marquee");
     const text = element.textContent;
     let speed = element.getAttribute("data-speed");
+    let clones = element.getAttribute("data-clones");
+
     speed = Number(speed);
+    clones = Number(clones);
 
     if (isNaN(speed) || speed < 1 || speed > 100) {
       speed = 20;
     }
 
-    const marquee = document.createElement("div");
+    if (isNaN(clones) || clones < 1) {
+      clones = 1;
+    }
 
+    const marquee = document.createElement("div");
+    marquee.style.display = "flex";
+    marquee.style.alignItems = "center";
+    marquee.style.flex = "1";
+    
     const marqueeP = document.createElement("p");
     marqueeP.innerHTML = text;
+    marqueeP.style.flex = "1";
+    marqueeP.style.marginRight = "-4px";
     if (direction === "right") {
       marqueeP.style.animation = "scroll-right " + speed + "s linear infinite";
     } else {
       marqueeP.style.animation = "scroll-left " + speed + "s linear infinite";
     }
     marquee.appendChild(marqueeP);
+
+    for (let i = 0; i < clones; i++) {
+      const marqueeClone = marqueeP.cloneNode(true);
+      marquee.appendChild(marqueeClone);
+    }
     element.parentNode.replaceChild(marquee, element);
   })
 }
