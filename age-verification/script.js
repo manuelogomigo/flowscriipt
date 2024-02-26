bb3bf46e85c4bc8293157da7f6298290982119a3
Lawwee <lawalmohammed567@gmail.com>
2024-02-26 14:03:23 +0100

    modified input to receive age and not dob

--- age-verification/script.js
+++ age-verification/script.js
@@ -1,23 +1,14 @@
 function verifyAge() {
     const elements = document.querySelectorAll("[data-dob]");
     elements.forEach((element) => {
-        const dob = element.value;
+        let age = element.value;
+        age = Number(age);
 
         const minAge = element.getAttribute("data-age");
         const url = element.getAttribute("data-url");
 
-        const dobDate = new Date(dob);
-
-        if (isNaN(dobDate.getTime())) {
-            alert("Please enter a valid date of birth");
-            return;
-        }
-
-        const today = new Date();
-        const age = today.getFullYear() - dobDate.getFullYear();
-
-        if (today.getMonth() < dobDate.getMonth() || (today.getMonth() == dobDate.getMonth() && today.getDate() < dobDate.getDate())) {
-            age--;
+        if (isNaN(age) || age < 1) {
+            alert("Please enter a valid age");
         }
 
         if (age >= minAge) {
